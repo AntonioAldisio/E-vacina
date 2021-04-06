2e4368f1c305b6847847abc168cd51e7234467d1
Antônio Aldísio <aldisiofilho@gmail.com>
2021-04-06 10:24:24 -0300

    Add files via upload

---
+++ E_vacina/app/src/main/java/mds/gams/evacina/ui/activity/Activity_InformacoesPessoais.java
@@ -0,0 +1,15 @@
+package mds.gams.evacina.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mds.gams.evacina.R;
+
+public class Activity_InformacoesPessoais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__informacoes_pessoais);
+    }
+}
---
+++ E_vacina/app/src/main/java/mds/gams/evacina/ui/activity/Activity_login.java
@@ -0,0 +1,24 @@
+package mds.gams.evacina.ui.activity;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import mds.gams.evacina.R;
+
+public class Activity_login extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
